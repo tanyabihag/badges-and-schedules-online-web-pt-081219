7ceba280a7049bd21bbd3f88e551f797db4866de
Tanya Bihag <tbihag@gmail.com>
2019-08-20 21:55:24 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  puts " Hi my name is #{name}"
+end
+
+badge_maker("Arel")
+
+def batch_badge_creator(array)
+  array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  speakers.each do |speakers|
+    puts "Welcome #{speakers}"
+end
+
+assign_rooms(batch_badge_creator)
+counter = 1 
+speakers.each do |speakers|
+  puts "#{speakers} you will stay in room #{counter}"
+  counter += 1
+end
+  
+  
+
