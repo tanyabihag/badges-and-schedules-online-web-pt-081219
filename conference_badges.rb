99f459ab03b5491e6de0e15c88c5640823286016
Tanya Bihag <tbihag@gmail.com>
2019-08-20 21:52:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,16 @@ def badge_maker(name)
 end
 
 badge_maker("Arel")
+
+def batch_badge_creator(array)
+  array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  speakers.each do |speakers|
+    puts "Welcome #{speakers}"
+end
+
+assign_rooms(batch_badge_creator)
+
+  speakers.each do |speakers|
+  
+  
+
